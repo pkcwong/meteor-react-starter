ece9617296b6db691f617162dc75e8848ea74a0a
Christopher Wong <christopherpk1996@gmail.com>
2018-07-14 16:07:28 +0800

    docs

---
+++ client/reducers/files-reducer/README.md
@@ -0,0 +1,6 @@
+## Files Saga
+
+dispatch type | payload | description
+--- | --- | ---
+```Files/UPLOAD-COMPLETE``` | <ul><li>file : FileCursor object</li></ul> | updates ```uploaded``` prop
+```Files/UPLOAD-RESET``` | ```null``` | empties the ```uploaded``` array prop
--- client/store.js
+++ client/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { all, fork } from 'redux-saga/effects';
-import { MeteorReducer } from "./reducers/meteor-reducer";
-import { FilesReducer } from "./reducers/files-reducer";
-import { MeteorSaga } from "./actions/meteor-saga";
-import { FilesSaga } from "./actions/files-saga";
+import { MeteorReducer } from "./reducers/meteor-reducer/meteor-reducer";
+import { FilesReducer } from "./reducers/files-reducer/files-reducer";
+import { MeteorSaga } from "./actions/meteor-saga/meteor-saga";
+import { FilesSaga } from "./actions/files-saga/files-saga";
 
 const saga = createSagaMiddleware();
 
