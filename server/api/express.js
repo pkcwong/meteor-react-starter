3747f9f81a9dd0494eadbfa7d0aaa5432c67227c
Christopher Wong <christopherpk1996@gmail.com>
2019-04-12 23:25:20 +0800

    upgraded meteor and rebuilds

--- server/api/express.js
+++ server/api/express.js
@@ -1,5 +1,8 @@
 import { WebApp } from 'meteor/webapp';
 import express from 'express';
+const cors = require('cors');
 
 export const app = express();
+app.use(cors());
+
 WebApp.connectHandlers.use(app);
